3e7ea0b262dbeda45a4e3675a3e82d282f554b71
gturaids <gturaids@gmail.com>
2024-02-07 19:59:21 -0800

    rebasing

---
+++ server/player.js
@@ -0,0 +1,14 @@
+/*
+This file holds all the game logic for the player portal
+
+-Provide game state information to front end for display (or will that be handled by the state.js route?)
+-When action is selected
+    -check if action is allowed
+    -check action type (e.g., move, pick up, enable, make visible)
+    -update game state
+    -Front end must now refresh
+
+*/
+
+'use strict'
+
