10dee0583f4bdd4caa3d5a2b12efeb29c770bd72
gturaids <gturaids@gmail.com>
2024-02-10 10:27:32 -0800

    Updated landing page text

--- client/src/components/landingpage/landingpage.js
+++ client/src/components/landingpage/landingpage.js
@@ -7,9 +7,19 @@ export default function LandingPage() {
       <Card.Body>
         <Card.Title>Text Adventure Landing Page</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+      	  <p>Welcome Adventurers, to ***Text Adventure*** for educators and students! Enrich the learning experience 
+		  with customized games for any topic or learning environment. All you need is an internet connection.</p>
+		  
+		  <p>Educators: Designed with you in mind, with ***Text Adventure*** you will create engaging 
+		  and immersive learning experiences. Our intuitive platform lets you easily create, edit, and share
+		  interactive games tailored to your curriculum.</p>
+
+		  <p>Students: Sick of "whiteboard" Jeopardy?  Prepare for a truly immersive experience. Whether you're exploring
+		  ancient civilizations, dissecting literary classics, or mastering mathematical concepts, ***Text Adventure*** allows 
+		  you to absorb these concepts at your own pace, and while having fun. </p>
+		  		  
+		  <p>Dive into a world where learning meets adventure. Join the adventure!</p>
+		</Card.Text>
       </Card.Body>
     </Card>
   );
