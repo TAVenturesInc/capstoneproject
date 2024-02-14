f3ec612e9c77e2ac6cc33177172014614e408399
gturaids <gturaids@gmail.com>
2024-02-14 13:58:44 -0800

    minor landing page edits

--- client/src/components/landingpage/landingpage.js
+++ client/src/components/landingpage/landingpage.js
@@ -5,20 +5,21 @@ export default function LandingPage() {
     <Card style={{ margin: "2rem 4rem" }}>
       <Card.Img />
       <Card.Body>
-        <Card.Title>Text Adventure Landing Page</Card.Title>
+        <Card.Title>TAVenture: A Text Adventure for Students and Teachers</Card.Title>
         <Card.Text>
-      	  <p>Welcome Adventurers, to ***Text Adventure*** for educators and students! Enrich the learning experience 
-		  with customized games for any topic or learning environment. All you need is an internet connection.</p>
-		  
-		  <p>Educators: Designed with you in mind, with ***Text Adventure*** you will create engaging 
-		  and immersive learning experiences. Our intuitive platform lets you easily create, edit, and share
-		  interactive games tailored to your curriculum.</p>
-
-		  <p>Students: Sick of "whiteboard" Jeopardy?  Prepare for a truly immersive experience. Whether you're exploring
-		  ancient civilizations, dissecting literary classics, or mastering mathematical concepts, ***Text Adventure*** allows 
-		  you to absorb these concepts at your own pace, and while having fun. </p>
-		  		  
-		  <p>Dive into a world where learning meets adventure. Join the adventure!</p>
+			<br></br><br></br>
+      		Welcome Adventurers, to TAVenture for educators and students! Enrich the learning experience 
+			with customized games for any topic or learning environment. All you need is an internet connection.
+			<br></br><br></br>
+			Educators: Designed with you in mind, with TAVenture you will create engaging 
+			and immersive learning experiences. Our intuitive platform lets you easily create, edit, and share
+			interactive games tailored to your curriculum.
+			<br></br><br></br>
+			Students: Sick of "whiteboard" Jeopardy?  Prepare for a truly immersive experience. Whether you're exploring
+			ancient civilizations, dissecting literary classics, or mastering mathematical concepts, TAVenture allows 
+			you to absorb these concepts at your own pace, and while having fun.
+			<br></br><br></br>  
+		  	Join the adventure!
 		</Card.Text>
       </Card.Body>
     </Card>
